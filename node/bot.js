3f1157a07863b724fab85c16f0d0b13fde169059
lopugit <ireply@myleisure.com.au>
2017-03-23 20:07:25 +1100

    added facebook node packages

--- node/bot.js
+++ node/bot.js
@@ -1,18 +1,14 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : 'your-app-id',
-    xfbml      : true,
-    version    : 'v2.8'
-  });
-  FB.AppEvents.logPageView();
-};
+var express = require('express');
+var app = express();
 
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+var path = require('path');
 
 // Be a bot
+
+app.get('/', function(req, res) {
+	res.send("botting");
+});
+
+var port = 5555;
+
+app.listen(port);
